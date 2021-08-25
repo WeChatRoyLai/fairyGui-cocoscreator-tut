0a6e2519e0b4163170bd0ff06680f9986c56f733
yifei yan <327147031@qq.com>
2021-08-25 20:38:48 +0800

    update

---
+++ demo/build/wechatgame/src/settings.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"wechatgame",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/FairyGUI-Demo.fire",orientation:"landscape",server:"",jsList:["assets/Script/Lib/fairygui.js"]};
